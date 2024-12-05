4c47b77934b9dedf35fe3fe6e06e1f7d17c83854
kapustakiszona <niahaibudze@gmail.com>
2024-12-05 21:42:18 +0100

    Basic bot logic with database

--- data_base/dao.py
+++ data_base/dao.py
@@ -2,7 +2,6 @@ import logging
 
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from data_base.base import connection
